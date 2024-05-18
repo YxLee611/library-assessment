31c20f5c395a8d88be72a10b709a39d88790e20b
Yx Lee <lxy6116@gmail.com>
2024-05-18 19:54:28 +0800

    Added >> basic setup vue

---
+++ resources/js/Pages/HomeRoute.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>HOME</h1>
+        <router-link to="/test"> Take me to Test page </router-link>
+        <!-- <p v-if="response">{{ response.data }}</p> -->
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import { ref } from "vue";
+
+// const response = ref();
+
+// const getValue = async () => {
+//     try {
+//         response.value = await axios.get("/api/test-me");
+//     } catch (error) {
+//         // Do something with the error
+//         console.log(error);
+//     }
+// };
+
+</script>
+
